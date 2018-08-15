03c361fa1fa13fdd5db93933bb323221dd27f331
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-08-15 02:13:42 -0400

    change to 4 array

--- array.rb
+++ array.rb
@@ -1,9 +1,36 @@
 def array_first()
 	my_arr = []
 	4.times do
-		my_arr.push(rand(9))
+		my_arr.push(rand(10))
+	end
+	p my_arr
+end
+
+def array_second()
+	my_arr = []
+	4.times do
+		my_arr.push(rand(10))
+	end
+	p my_arr
+end
+
+def array_third()
+	my_arr = []
+	4.times do
+		my_arr.push(rand(10))
+	end
+	p my_arr
+end
+
+def array_fourth()
+	my_arr = []
+	4.times do 
+		my_arr.push(rand(10))
 	end
 	p my_arr
 end
 
 array_first()
+array_second()
+array_third()
+array_fourth()
