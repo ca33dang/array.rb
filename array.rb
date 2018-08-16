badbabd507cc98bbd2ffaaabcfcd9d8c019ffbfa
ca33dang <38273466+ca33dang@users.noreply.github.com>
2018-08-15 21:51:15 -0400

    add element to array

--- array.rb
+++ array.rb
@@ -2,35 +2,19 @@ def array_first()
 	my_arr = []
 	4.times do
 		my_arr.push(rand(10))
+		n = 9
+		my_array = Array.new(n)
+		i = 0
+		loop do
+			random_number = rand(n+1)
+			my_arr.push(random_number)
+
+			i += 1
+			break if i >= n
+		end
 	end
 	p my_arr
 end
 
-def array_second()
-	my_arr = []
-	4.times do
-		my_arr.push(rand(10))
-	end
-	p my_arr
-end
-
-def array_third()
-	my_arr = []
-	4.times do
-		my_arr.push(rand(10))
-	end
-	p my_arr
-end
-
-def array_fourth()
-	my_arr = []
-	4.times do 
-		my_arr.push(rand(10))
-	end
-	p my_arr
-end
 
 array_first()
-array_second()
-array_third()
-array_fourth()
